7df97200f9c899251821f81f1a00d589a3d0d041
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2020-02-09 13:40:16 +0200

    add o(n) for remove duplicate

--- programming/basics/crackingthecodinginterview/linked_lists/main.py
+++ programming/basics/crackingthecodinginterview/linked_lists/main.py
@@ -7,12 +7,12 @@ class LinkedList:
     def __init__(self, head=None):
         self.head = head
     
-    def remove_duplicate(self):
+    def remove_duplicate_n2(self):
         current = self.head
-        counter = 0
         while current:
             iterator = head
             before = iterator
+            counter = 0
             while iterator:
                 if iterator.val == current.val:
                     counter += 1
@@ -25,8 +25,30 @@ class LinkedList:
                 before = iterator
                 iterator = iterator.next_node
             
-            counter = 0
             current = current.next_node
+    
+    def remove_duplicate_n(self):
+        current = self.head
+        counter = {}
+        while current:
+            if current.val in counter:
+                counter[current.val] += 1
+            else:
+                counter[current.val] = 1
+                
+            current = current.next_node
+        
+        current = self.head.next_node
+        before = self.head
+        while current:
+            if counter[current.val] > 1:
+                before.next_node = current.next_node
+                counter[current.val] -= 1
+                current = None
+                current = before.next_node
+            else:
+                before = current
+                current = current.next_node
 
     def print_all(self):
         current = self.head
@@ -37,12 +59,15 @@ class LinkedList:
 head = Node(9)
 a = Node(9)
 b = Node(7)
+c = Node(9)
 
 head.next_node = a
 a.next_node = b
+b.next_node = c
 
 l = LinkedList(head)
 l.print_all()
-l.remove_duplicate()
+# l.remove_duplicate_n2()
+l.remove_duplicate_n()
 print("---")
 l.print_all()
